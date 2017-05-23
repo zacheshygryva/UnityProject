bd66b18ff13e40b7eafba82ea91183561be558ac
Balerina <Balerina>
2017-05-24 02:02:59 +0300

    Lab1

---
+++ Assets/Content/Rabit/HeroFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour {
+
+	public HeroControll rabit;
+
+	void Update () {
+		
+		Transform rabit_transform = rabit.transform;
+
+		Transform camera_transform = this.transform;
+
+		Vector3 rabit_position = rabit_transform.position;
+		Vector3 camera_position = camera_transform.position;
+
+		camera_position.x = rabit_position.x;
+		camera_position.y = rabit_position.y;
+
+		camera_transform.position = camera_position;
+	}
+}
